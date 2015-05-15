e19befda88617df669b2637d34e24d8c33fca9b6
Jason Dorn <jdorn@mail.sfsu.edu>
2015-05-14 18:08:38 -0700

    Added dogs resource

---
+++ app/models/dog.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  breed: DS.attr('string'),
+  age: DS.attr('number'),
+  size: DS.attr('string'),
+  trained: DS.attr('boolean'),
+  animalFriendly: DS.attr('boolean'),
+  childFriendly: DS.attr('boolean'),
+  bio: DS.attr(),
+  sex: DS.attr('string'),
+  spayedOrNeutered: DS.attr('boolean')
+});
